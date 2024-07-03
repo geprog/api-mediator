ee9718ad3c167d577ebc2057c3041356a431d4ec
Marius Heine <m.heine@geprog.com>
2024-07-03 12:20:04 +0200

    enable cors

--- mock-app/index.ts
+++ mock-app/index.ts
@@ -1,9 +1,11 @@
 import { Hono } from 'hono';
+import { cors } from 'hono/cors'
 import github from './routes/github';
 import gitlab from './routes/gitlab';
 import { serve } from 'bun';
 
 const app = new Hono();
+app.use('*', cors());
 
 app.get('/', (c) => {
   return c.text('Hello World!');
@@ -13,3 +15,4 @@ app.route('/github', github);
 app.route('/gitlab', gitlab);
 
 serve({ fetch: app.fetch, port: 8787 });
+console.log('Mock is running');
