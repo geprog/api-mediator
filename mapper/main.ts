bcefe505b6aa57103e5bc464c20bcce5bc7c2e28
Marius Heine <m.heine@geprog.com>
2024-07-03 15:59:28 +0200

    take baseUrl from source and target api

--- mapper/main.ts
+++ mapper/main.ts
@@ -12,32 +12,67 @@ async function main() {
   );
 
   // await generateMapping(); // githubApi, gitlabApi);
-  const mapping = { sourceAPI: githubApi, targetAPI: gitlabApi, source: { name: 'github-get', path: 'http://localhost:8787/github/issue', method: 'GET' }, target: { name: 'gitlab-post', path: 'http://localhost:8787/gitlab/issues', method: 'POST' }, mapping: { id: 'id', name: 'title', body: 'description', closed: 'closed' } }
+  const mapping = {
+    sourceAPI: githubApi,
+    targetAPI: gitlabApi,
+    source: {
+      name: 'github-get',
+      path: '/issue',
+      method: 'GET',
+    },
+    target: {
+      name: 'gitlab-post',
+      path: '/issues',
+      method: 'POST',
+    },
+    mapping: {
+      id: 'id',
+      name: 'title',
+      body: 'description',
+      closed: 'closed',
+    },
+  };
 
   setInterval(async () => {
     // sync the data
-    const sourceEndpoint = mapping.source;
-    const targetEndpoint = mapping.target;
     // 1. fetch endpoint from source
-    const sourceResponse = await fetch(mapping.source.path, { method: mapping.source.method });
-    const sourceItems = await sourceResponse.json() as Record<string, unknown>[];
+    const sourceResponse = await fetch(
+      `${mapping.sourceAPI.baseUrl}${mapping.source.path}`,
+      {
+        method: mapping.source.method,
+      },
+    );
+    const sourceItems = (await sourceResponse.json()) as Record<
+      string,
+      unknown
+    >[];
     console.log(`sycing ${sourceItems.length} items`);
 
     // 2. map properties
     for (const sourceItem of sourceItems) {
       console.log('handling sourceItem ', sourceItem);
-      const targetItem = Object.entries(mapping.mapping).reduce<Record<string, unknown>>((item, [sourceProp, targetProp]) => (
-        { ...item, [targetProp]: sourceItem[sourceProp] }
-      ), {})
+      const targetItem = Object.entries(mapping.mapping).reduce<
+        Record<string, unknown>
+      >(
+        (item, [sourceProp, targetProp]) => ({
+          ...item,
+          [targetProp]: sourceItem[sourceProp],
+        }),
+        {},
+      );
       console.log('mapped to targetItem ', targetItem);
 
       // 3. execute endpoint at target
-      const response = await fetch(mapping.target.path, { method: mapping.target.method, body: JSON.stringify(targetItem), headers: { "Content-Type": "application/json" } });
+      const response = await fetch(
+        `${mapping.targetAPI.baseUrl}${mapping.target.path}`,
+        {
+          method: mapping.target.method,
+          body: JSON.stringify(targetItem),
+          headers: { 'Content-Type': 'application/json' },
+        },
+      );
       console.log('saved item ', await response.json());
     }
-
-    // const syncData = mapping.source.responseSchema.fields.map((field) => mapping.mapping[field.name]);
-
   }, 5000);
 }
 
